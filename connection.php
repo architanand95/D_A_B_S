5cbfac84d00e5b018c5b501924ef3201df0652b1
AYUSH SISODIYA <148615084+ASR0228@users.noreply.github.com>
2023-11-23 09:24:09 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","PES2UG21CS088","edoc");
+    $database= new mysqli("localhost","root","PES2UG21CS108","edoc");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
